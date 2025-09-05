0317712bd39dc535223b834cada7ff3a5cc3eb1d
Kurtisatson <kt332224@ohio.edu>
2025-09-05 15:11:26 -0400

    This is my program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Kurtis Thompson
+ *        Date: September 5th, 2025
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Code here often?" << endl;
+    return 0;
+}// main
